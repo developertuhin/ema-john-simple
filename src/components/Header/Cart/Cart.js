aff603aed930234efd2f332dbe0551f0d116ac97
Tuhin Khan <webwithjhankarvai@gmail.com>
2021-02-28 16:53:13 +0600

    Maximum done

--- src/components/Header/Cart/Cart.js
+++ src/components/Header/Cart/Cart.js
@@ -9,14 +9,30 @@ const Cart = (props) => {
     let total = 0;
     for(let i=0; i<cart.length; i++){
         const product = cart[i];
-        total = total + product.price
-
+        total = total + product.price;
+        total.toFixed(2);
     }
-    return (
+    let shipping = 0;
+    if (total > 100) {
+        shipping = 0;
+    }
+    else if (total > 35) {
+        shipping = 4.99;
+    }
+    else if (total > 15) {
+        shipping = 12.33;
+    }
+    
+    let vat = (total / 10).toFixed(2);
+    let grandTotal = ((total + shipping + Number(vat)).toFixed(2));
+ return (
         <div>
            <h2>Order Summery</h2> 
            <p>Items Ordered : {cart.length}</p>
-           <p>Total Price : $ {total}</p>
+           <p>Product Price : {total}</p>
+           <p><small>Shipping :$ {shipping}</small></p>
+           <p><small>Vat :$ {vat}</small></p>
+           <p>Total Price : $ {grandTotal}</p>
         </div>
     );
 };
